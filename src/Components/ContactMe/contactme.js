26cb56f3d3eb8139ad3d796161022458de0e44d5
Zain Mukhtar <67791976+zaindmdmax@users.noreply.github.com>
2021-03-13 17:40:56 +0500

    minor changesss
    sss

--- src/Components/ContactMe/contactme.js
+++ src/Components/ContactMe/contactme.js
@@ -66,7 +66,7 @@ export default function contactme() {
             name="message"
           />
           <Button className="contactBtn" type="submit">
-            Submit
+            Submittt
           </Button>
         </Form>
       </Segment>
